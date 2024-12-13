881d7aabffac9da3020a35fe3ed9c34534dd2649
drkmatheus <matheus.gcrosciati@gmail.com>
2024-12-13 17:22:18 -0300

    Implementado classe Seller

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,17 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dep = new Department(1, "Hardware");
-        System.out.println(dep);
+
+
+        Seller seller = new Seller(006, "Foo", "teste@mail.com", new Date(), 2200.0, dep);
+
+        System.out.println(seller);
     }
 }
