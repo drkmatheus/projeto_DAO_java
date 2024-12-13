5bcda03d2850291c9cd2bbbd6a7ed2f05eee5cd7
drkmatheus <matheus.gcrosciati@gmail.com>
2024-12-13 17:59:14 -0300

    Implementado findById

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,19 +2,14 @@ package app;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department dep = new Department(1, "Hardware");
-
-
-        Seller seller = new Seller(006, "Foo", "teste@mail.com", new Date(), 2200.0, dep);
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); //chamando o daofactory eu nao exponho o codigo fonte
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
